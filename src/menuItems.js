2fa9af0d2b84ca7851624ac205699f388f8977dc
Michael M <chewedgum12@gmail.com>
2022-07-09 18:44:06 -0500

    add data for menu items

---
+++ src/menuItems.js
@@ -0,0 +1,100 @@
+const menuItems = {
+    'Appetizers': [
+        {
+            name: 'APPETIZER SAMPLER / Khai Vị Đặc Biệt',
+            description: '(2) Spring Rolls, (2) SIMPLY PHỞ Egg Rolls, (2) Shrimp Sausage Wrap, and (4) Shrimp Wrap'
+        },
+        {
+            name: 'VEGETARIAN SPRING ROLLS (2) / Gỏi Cuốn Chay',
+            description: 'Rice paper freshly wrapped around Tofu, iceberg lettuce, basil, beansprouts and vermicelli served with house special peanut sauce'
+        },
+        {
+            name: 'EDAMAME'
+        },
+        {
+            name: 'FRIED OR STEAMED DUMPLING (5)',
+            description: 'An irresistible dumplings filled with ground pork and vegetables served with special dumpling sauce.'
+        }
+    ],
+    'PHỞ': [
+        {
+            name: 'KOBE BEEF Phở',
+        },
+        {
+            name: 'FILET MIGNON Phở'
+        },
+
+        {
+            name: 'SHRIMP PHO / Phở Tôm'
+        },
+        {
+            name: 'PHỞ SPECIAL / Đặc Biệt',
+            description: 'A hearty combination of eye-round steak, well-done flank, tripe, beef meatballs, well-done brisket, and soft tendon.'
+        },
+        {
+            name: 'EYE ROUND STEAK PHO / Phở Tái'
+        },
+        {
+            name: 'VEGETABLE PHO / Phở Rau Cải',
+            description: '*Vegetable broth available upon request'
+        },
+        {
+           name: '>SEAFOOD PHO/ Phở Hải Vị',
+           description: 'Combination of shrimp, calamari, imi. crab meat , and fish paste balls.' 
+        },
+        {
+            name: 'YOUR CREATION / Phở Như Ý',
+            description: 'Pick up to three toppings: Eye round steak, well-done brisket, well-done flank, soft tendon, tripe, or beef meatballs.'
+        }
+    ],
+    'VERMICELLI or ANGEL HAIR / Bún /Bánh Hỏi': [
+        {
+            name: 'VERMICELLI COMBINATION',
+            description: 'Combination of SIMPLY PHỞ egg roll, grilled pork, grilled shrimp, pork sausage, and shrimp sausage wrap.'
+        },
+        {
+            name: 'GRILLED PORK / Thịt Nướng'
+        },
+        {
+            name: 'GRILLED CHICKEN / Gà Nướng'
+        },
+        {
+            name: 'GRILLED BEEF / Bò Nướng'
+        },
+        {
+            name: 'GRILLED SHRIMP / Tôm Nướng'
+        },
+        {
+            name: 'SAUTÉED BEEF AND ONIONS / Bún Bò Xào'
+        },
+        {
+            name: 'YOUR CREATION',
+            description: 'Choose up to three items: Grilled Pork, Grilled Chicken, Grilled Beef, Grilled Shrimp , Charbroiled Pork Sausage, or SIMPLY PHỞ Egg Roll.'
+        },
+        {
+            name: 'SAUTÉED TOFU'
+        }
+    ],
+    'Drinks':[
+        {
+            name: 'VIETNAMESE ESPRESSO (served Hot or Iced) /Cà Phê Đen/Đen Đá'
+        },
+        {
+            name: 'VIETNAMESE ESPRESSO with CONDENSED MILK Cà Phê Sữa/Sữa Đá (served Hot or Iced)'
+        },
+        {
+            name: 'COCONUT JUICE / Nước Dừa Tươi'
+        },
+        {
+            name: 'SALTY PLUM SODA / Soda Xí Muội'
+        },
+        {
+            name: 'LYCHEE DRINK / Nước Trái Vải'
+        },
+        {
+            name: 'THAI TEA / Trà Thái'
+        }
+    ]
+}
+
+export { menuItems }
