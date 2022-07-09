ddab4d5701b239a5a01dfce794923503e98e4fdc
Selt0 <chewedgum12@gmail.com>
2022-07-09 14:53:32 -0500

    add image rule

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,12 @@ module.exports = {
             {
                 test:/\.css$/i,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                include: /images/,
+                type: 'asset/resource',
+              },
         ]
     }
 }
