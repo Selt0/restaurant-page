b4080ce30cc2230884658d6b1e4702fb71d33af8
Michael M <chewedgum12@gmail.com>
2022-07-09 18:47:24 -0500

    add modules and complete loadPage function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import {createHeader} from './header'
-import {createHomepage} from './home'
+import { createHeader } from './header'
+import { setNavActiveItem } from './nav'
+import { createHomepage } from './home'
+import { createMenu } from './menu'
+import { createStory } from './story'
+import { createFooter } from './footer'
 import './style.css'
 
 const contentContainer = document.querySelector('#content')
@@ -12,15 +16,26 @@ function initialPageLoad(){
     document.querySelector('.nav-item').classList.add('active')
 
     contentContainer.appendChild(createHomepage())
+    contentContainer.appendChild(createFooter())
 }
 
 function loadPage(e){
+    // do nothing unless user clicks directly on nav item
+    if (!e.target.classList.contains('nav-item')) return
     // do nothing if user clicks on nav item showing current page
-    if (e.target.classList.contains('active')){
-        return
-    }
+    if (e.target.classList.contains('active')) return
 
-    console.log(e.target.textContent)
+    document.querySelector('section').remove()
+    const footer = document.querySelector('footer')
+    setNavActiveItem(e.target)
+
+    if (e.target.textContent === 'Menu'){
+        contentContainer.insertBefore(createMenu(), footer)
+    } else if (e.target.textContent === 'Home'){
+        contentContainer.insertBefore(createHomepage(), footer)
+    } else {
+        contentContainer.insertBefore(createStory(), footer)
+    }
 }
 
 initialPageLoad()
